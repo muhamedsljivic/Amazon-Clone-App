45a2c6114b9807419795ca936de529baa1f2d13c
Muhamed Sljivic <muhamedica09@gmail.com>
2022-11-23 12:11:50 +0100

    upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:amazon_clone/router.dart';
 import 'package:flutter/material.dart';
+
+import 'features/auth/screens/auth_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,18 +17,33 @@ class MyApp extends StatelessWidget {
       title: 'Amazon Clone',
       theme: ThemeData(
         scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme:
+            const ColorScheme.light(primary: GlobalVariables.secondaryColor),
         appBarTheme: const AppBarTheme(
           elevation: 0,
           iconTheme: IconThemeData(color: Colors.black),
         ),
       ),
+      onGenerateRoute: (settings) => generateRoute(settings),
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Hello."),
           centerTitle: true,
         ),
-        body: const Center(
-          child: Text('Flutter Demo Home Page'),
+        body: Column(
+          children: [
+            const Center(
+              child: Text('Flutter Demo Home Page'),
+            ),
+            Builder(builder: (context) {
+              return ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, AuthScreen.routeName);
+                },
+                child: const Text("Click"),
+              );
+            }),
+          ],
         ),
       ),
     );
